5e9a4694286d1bbed5e9cbf7f2fcd5386c32d6dd
shepherd <1059959730@qq.com>
2022-02-23 01:05:07 +0800

    add:添加bean元信息配置demo

---
+++ spring-shepherd/src/main/java/com/shepherd/bean/spring/lifecycle/BeanMetadataConfigurationDemo.java
@@ -0,0 +1,31 @@
+package com.shepherd.bean.spring.lifecycle;
+
+import com.shepherd.domain.User;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.EncodedResource;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2022/2/23 0:49
+ */
+public class BeanMetadataConfigurationDemo {
+	public static void main(String[] args) {
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		// 实例化基于 Properties 资源 BeanDefinitionReader
+		PropertiesBeanDefinitionReader beanDefinitionReader = new PropertiesBeanDefinitionReader(beanFactory);
+		String location = "user.properties";
+		// 基于 ClassPath 加载 properties 资源
+		Resource resource = new ClassPathResource(location);
+		// 指定字符编码 UTF-8
+		EncodedResource encodedResource = new EncodedResource(resource, "UTF-8");
+		int beanNumbers = beanDefinitionReader.loadBeanDefinitions(encodedResource);
+		System.out.println("已加载 BeanDefinition 数量：" + beanNumbers);
+		// 通过 Bean Id 和类型进行依赖查找
+		User user = beanFactory.getBean("user", User.class);
+		System.out.println(user);
+	}
+}
